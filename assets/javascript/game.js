e343483c7f4297882bb65b70bece9c9dd9af1b37
Mollie <molliechindavong@gmail.com>
2017-06-17 23:27:40 -0400

    adding files

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+ 					   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var yourGuesses = "";
+
+
+document.onkeyup = function(event) {
+
+	var userGuess = event.key;
+	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+	if (userGuess === computerGuess > 0) {
+          wins++;
+          guessesLeft--;
+        }
+
+      else if (guessesLeft > 0) {
+        losses++;
+        guessesLeft--;
+      }
+
+       var html =
+          "<h2>WINS: " + wins + "</h2>" +
+          "<h2>LOSSES: " + losses + "</h2>" +
+          "<h2>GUESSES LEFT: " + guessesLeft + "</h2>" +
+          "<h2>YOUR GUESSES SO FAR: " + userGuess + "</h2>";
+
+          document.querySelector(".game").innerHTML = html;
+
+      if (guessesLeft === 0) {
+        document.querySelector(".game").innerHTML = "<h2>YOU GUESSED " + wins + " OUT OF 10 LETTERS</h>" ;
+        return
+
+        }
+
+}
+
